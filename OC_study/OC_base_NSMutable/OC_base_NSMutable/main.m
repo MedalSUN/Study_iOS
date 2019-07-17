55e7bde7af2d1a1445082b2cdbe0eb19ddb95b67
孙栋梁 <2496667187@qq.com>
2019-07-17 22:38:30 +0800

    OC中的可变类型和不可变类型

---
+++ OC_study/OC_base_NSMutable/OC_base_NSMutable/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_base_NSMutable
+//
+//  Created by SunDL on 2019/7/17.
+//  Copyright © 2019 FZF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
